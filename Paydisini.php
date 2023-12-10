924a069e5f58c6dad3919afa852cf86475dd1c6c
Rud Az <36195757+Lyaxpm@users.noreply.github.com>
2023-12-10 23:42:45 +0700

    Add files via upload

---
+++ Paydisini.php
@@ -0,0 +1,102 @@
+<?php
+/* ==========================================
+  | Author : Rud Az
+  |
+  | Paydisini Lib PHP Class
+  */
+
+class Paydisini {
+  const URL = 'https://paydisini.co.id/api/';
+
+  static $apiKey;
+
+  static $cmd = [
+    'order' => 'NewTransaction',
+    'status' => 'StatusTransaction',
+    'cancel' => 'CancelTransaction',
+    'chanel' => 'PaymentChannel',
+    'guide' => 'PaymentGuide',
+    'call' => 'CallbackStatus'
+  ];
+
+  public function config($params) {
+    Paydisini::$apiKey = $params['apiKey'];
+  }
+
+  public function transaction($code, $service, $amount, $wallet = null) {
+    return Paydisini::Request(self::URL, [
+      'key' => Paydisini::$apiKey,
+      'request' => 'new',
+      'unique_code' => $code,
+      'service' => $service,
+      'amount' => $amount,
+      'note' => 'Request new order',
+      'valid_time' => 10800,
+      'ewallet_phone' => $wallet,
+      'type_fee' => 2,
+      'signature' => md5(
+        Paydisini::$apiKey .
+        $code .
+        $service .
+        $amount .
+        '10800' .
+        self::$cmd['order']
+      )
+    ]);
+  }
+
+  public function status($code) {
+    return Paydisini::Request(self::URL, [
+      'key' => Paydisini::$apiKey,
+      'request' => 'status',
+      'unique_code' => $code,
+      'signature' => md5(
+        Paydisini::$apiKey .
+        $code .
+        self::$cmd['status']
+      )
+    ]);
+  }
+
+  public function chanel() {
+    return Paydisini::Request(self::URL, [
+      'key' => Paydisini::$apiKey,
+      'request' => 'payment_channel',
+      'signature' => md5(
+        Paydisini::$apiKey .
+        self::$cmd['chanel']
+      )
+    ]);
+  }
+
+  public function guide($service) {
+    return Paydisini::Request(self::URL, [
+      'key' => Paydisini::$apiKey,
+      'request' => 'payment_guide',
+      'service' => $service,
+      'signature' => md5(
+        Paydisini::$apiKey .
+        $service .
+        self::$cmd['guide']
+      )
+    ]);
+  }
+
+  public static function Request($url, $params = []) {
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_POST, 1);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($params));
+curl_setopt($curl, CURLOPT_ENCODING, 'gzip, deflate');
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+    if (curl_errno($curl)) {
+      throw new \Exception('[ERROR] Cannot get server response!');
+    } else {
+      $result = curl_exec($curl);
+      curl_close($curl);
+      $response = json_decode($result, true);
+      return $response;
+    }
+  }
+}
